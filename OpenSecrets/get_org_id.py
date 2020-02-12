90ee159743f26c07a0f350e08ee530686651be7c
Yvonne Han <yvonnehanyz@gmail.com>
2020-02-12 14:58:23 +1100

    Second attempt.

--- OpenSecrets/get_org_id.py
+++ OpenSecrets/get_org_id.py
@@ -34,10 +34,13 @@ def get_org_id(some_text, api_key):
 # sample call: starbucks
 # get_org_id("starbucks")
 
-# first attempt:  found ids for 368/498 comnams 
+# create a new dataframe with org_id and org_id_candidates as blank
 ticker_comnam_id = pd.read_csv("ticker_comname.csv")
 ticker_comnam_id['org_id'] = None
 ticker_comnam_id['org_id_candidates'] = None
+
+# First attempt: use the normal clean_comnam as search keywords
+# Found ids for 368/498 comnams
 
 for i in range(len(ticker_comnam_id)):
     donor_name = ""
@@ -59,3 +62,32 @@ for i in range(len(ticker_comnam_id)):
     print(i, org_id_result)
 
 ticker_comnam_id.to_csv("ticker_comnam_id.csv", encoding='utf-8', index=False)
+
+# hand-collected for very short comnams: 394/498 matches (79%)
+
+# Second attempt: use the first word in the comnam to get a candidate list of possible matches
+# To allow for manual judgment, I put all results in candidates no matter how many are returned for each query
+# Found 380/398 ids
+ticker_comnam_id_second = pd.read_csv("ticker_comnam_id.csv")
+
+for i in range(len(ticker_comnam_id_second)):
+    donor_name = ""
+    candidates = []
+    
+    # set your own key for this
+    api_key = '7c4252bc145851b63185f71a96e7623a'
+    
+    # only deal with those with blanks for both org_id and org_id_candidates
+    if not isinstance(ticker_comnam_id_second.loc[i, 'org_id_candidates'], str) and not isinstance(ticker_comnam_id_second.loc[i, 'org_id'], str):
+        comnam = clean_comnam(ticker_comnam_id_second['comnam'][i].split(" ")[0])
+        print(i, comnam)
+
+        org_id_result = get_org_id(comnam, api_key)
+
+        if org_id_result:
+            if ';' in org_id_result:
+                ticker_comnam_id_second.loc[i,'org_id_candidates'] = org_id_result
+            else:
+                ticker_comnam_id_second.loc[i,'org_id'] = org_id_result
+
+ticker_comnam_id_second.to_csv("ticker_comnam_id.csv", encoding='utf-8', index=False)
